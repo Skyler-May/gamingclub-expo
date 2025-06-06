c132ce279fa0efa0e886323df462532f6ab4e53f
Skyler-May <meisongyong@gmail.com>
2025-06-06 18:10:44 +0800

    Initial commit

--- constants/Profile/ToolCardsData.ts
+++ constants/Profile/ToolCardsData.ts
@@ -1,5 +1,5 @@
 // 定义路由路径类型
-type RoutePaths = "/profile/fuli" | "/profile/chat";
+type RoutePaths = "/fuli" | "/chat";
 
 // 定义工具项的接口
 export interface DataItem {
@@ -15,31 +15,31 @@ export const TOOLS_DATA: DataItem[] = [
     icon: "gift",
     text: "福利",
     color: "#F54A00",
-    route: "/profile/fuli",
+    route: "/fuli",
   },
   {
     icon: "wechat",
     text: "聊天室",
     color: "#F54A00",
-    route: "/profile/chat",
+    route: "/chat",
   },
   //   {
   //     icon: "dollar-sign",
   //     text: "买卖TRbi",
   //     color: "#F54A00",
-  //     route: "/profile/payments",
+  //     route: "/payments",
   //   },
   //   {
   //     icon: "users",
   //     text: "代理",
   //     color: "#F54A00",
-  //     route: "/profile/agent",
+  //     route: "/agent",
   //   },
   //   {
   //     icon: "credit-card",
   //     text: "钱包",
   //     color: "#F54A00",
-  //     route: "/profile/payments",
+  //     route: "/payments",
   //   },
 ];
 
@@ -49,19 +49,19 @@ export const CHANNELS_DATA: DataItem[] = [
   //     icon: "bell",
   //     text: "公告",
   //     color: "#F54A00",
-  //     route: "/profile/announcement",
+  //     route: "/announcement",
   //   },
   //   {
   //     icon: "message-circle",
   //     text: "聊天",
   //     color: "#F54A00",
-  //     route: "/profile/chat",
+  //     route: "/chat",
   //   },
   //   {
   //     icon: "gift",
   //     text: "福利",
   //     color: "#F54A00",
-  //     route: "/profile/fuli",
+  //     route: "/fuli",
   //   },
 ];
 
@@ -71,18 +71,18 @@ export const PLATFORM_INFO: DataItem[] = [
   //     icon: "globe",
   //     text: "全球",
   //     color: "#F54A00",
-  //     route: "/profile/global",
+  //     route: "/global",
   //   },
   //   {
   //     icon: "users",
   //     text: "100000+",
   //     color: "#F54A00",
-  //     route: "/profile/users",
+  //     route: "/users",
   //   },
   //   {
   //     icon: "dollar-sign",
   //     text: "100000+",
   //     color: "#F54A00",
-  //     route: "/profile/transactions",
+  //     route: "/transactions",
   //   },
 ];
