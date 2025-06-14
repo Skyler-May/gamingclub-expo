683d6244c4e1dafa62500fef40c24cd4e9ae5726
Skyler-May <meisongyong@gmail.com>
2025-06-14 15:42:02 +0800

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,58 +1,9 @@
-import { QuickButton } from "@/components/QuickButton";
-import { useDailyCountdown } from "@/hooks/useCountdown";
-import React from "react";
-import { Modal, StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
-export default function CountdownScreen() {
-  const { status, hours, minutes, seconds } = useDailyCountdown({
-    startTime: { hour: 15, minute: 20, second: 0 },
-    endTime: { hour: 15, minute: 25, second: 0 },
-  });
-
-  const format = (n: number) => n.toString().padStart(2, "0");
-
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      {status === "counting" ? (
-        <Text style={styles.text}>
-          距离 21:00 倒计时：{format(hours)}:{format(minutes)}:{format(seconds)}
-        </Text>
-      ) : (
-        <Modal visible={true} transparent animationType="fade">
-          <View style={styles.modalBackground}>
-            <View style={styles.modalBox}>
-              <Text style={styles.modalText}>已到 21:00，休息中...</Text>
-              <Text style={styles.modalText}>
-                距离下轮还有 {format(hours)}:{format(minutes)}:{format(seconds)}
-              </Text>
-            </View>
-          </View>
-        </Modal>
-      )}
-      <QuickButton />
+    <View>
+      <Text>HomeScreen</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-  },
-  modalBackground: {
-    flex: 1,
-    backgroundColor: "#00000088",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  modalBox: {
-    backgroundColor: "white",
-    padding: 24,
-    borderRadius: 12,
-  },
-  modalText: { fontSize: 18, marginBottom: 8 },
-});
